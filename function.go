bee708c9bf79eb2f172b62e4d902c9de8596b72a
halokitor <13672626943@163.com>
2023-08-14 00:52:08 +0800

    add: add funtion tutorial

---
+++ function.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+/* function for two num add */
+func addNum_(x, y int) int {
+	return x + y
+}
+
+/* function for two value swap */
+func swapChar_(x, y string) (string, string) {
+	return y, x
+}
+
+/* function for return struct */
+func split_(param float32) (x, y float32) {
+	x = param*3.0 + 3.0
+	y = float32(math.Sqrt(float64(param)))
+	return
+}
+
+func main() {
+	/* terminal print add result */
+	fmt.Println("Hello Golang!\n", "The next words!")
+	fmt.Println("add num is", addNum_(9, 8))
+
+	charA := "charA"
+	charB := "charB"
+	charA, charB = charB, charA
+
+	a, b := swapChar_("charA", "charB")
+	fmt.Println("swap char reuslt", a, b, charA, charB)
+
+	fmt.Println(split_(8))
+}
