f18e28940ddbd4732534ad67fc8f54613de3b249
halokitor <13672626943@163.com>
2023-08-14 00:52:25 +0800

    add: add import package tutorial

---
+++ importPkg.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"math/rand"
+)
+
+func main() {
+	fmt.Println("Import Package, random num is ", rand.Intn(3))
+	fmt.Println("Import Package, sqrt num is ", math.Sqrt(9))
+	fmt.Println("Import Package, math Pi is ", math.Pi)
+}
